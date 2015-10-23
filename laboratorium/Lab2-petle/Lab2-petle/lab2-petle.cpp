bf855cd5934ba52ceb57120f9ea04eb24abdc59b
dochman <daniel.ochman95@gmail.com>
2015-10-23 20:22:06 +0200

    dodane odchylenie

--- laboratorium/Lab2-petle/Lab2-petle/lab2-petle.cpp
+++ laboratorium/Lab2-petle/Lab2-petle/lab2-petle.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <ctime>
+#include <cmath>
 
 using namespace std;
 
@@ -16,6 +17,8 @@ int main(int argc, char**argv)
 		cout << i << "\n";
 	}*/
 
+
+
 	// ZAD2, wyswietlenie n-liczb po kolei ale od n
 	/*int n, i;
 	cout << "Podaj n (tyle liczb wyswietli sie malejaco)" << endl;
@@ -26,7 +29,9 @@ int main(int argc, char**argv)
 		cout << i << "\n";
 	}*/
 
-    //ZAD3, wyswietlenie liczb z podanego przedzialu i podzielnych przez dzielnik
+
+
+	//ZAD3, wyswietlenie liczb z podanego przedzialu i podzielnych przez dzielnik
 	/*int start, stop, dzielnik;
 	cout << "wypisz poczatek przedzialu" << endl;
 	cin >> start;
@@ -44,6 +49,8 @@ int main(int argc, char**argv)
 	}
 	*/
 
+
+
 	//ZAD4, podniesienie do potegi
 	/*int  wykladnik;
 	double podstawa, wynik = 1;
@@ -58,6 +65,8 @@ int main(int argc, char**argv)
 	}
 	cout << wynik << endl;
 	*/
+
+
 
 	//ZAD5, czy liczba jest lb pierwsza
 	/*int liczba;
@@ -86,7 +95,9 @@ int main(int argc, char**argv)
 	}
 	*/
 
-	//ZAD6, zgadywanka w petli bez ograniczen
+
+
+	//ZAD6a, zgadywanka w petli bez ograniczen, for
 	/*int liczba, proba;
 	srand(time(NULL));
 	liczba = rand() % 21;
@@ -110,7 +121,55 @@ int main(int argc, char**argv)
 		}
 	}*/
 
-    //ZAD7, zgadywanka z ograniczeniem
+
+
+	//ZAD6b, zgadywanka w petli bez ograniczen, while
+	/*int liczba, proba;
+	srand(time(NULL));
+	liczba = rand() % 21;
+	cout << "Zgadnij jaka wylosowalem liczbe (0-20)" << endl;
+	cin >> proba;
+	while (proba != liczba)
+	{
+		if (proba > liczba)
+		{
+			cout << "Podana liczba jest wieksza od wylosowanej" << endl;
+		}
+		else if (proba < liczba)
+		{
+			cout << "Podana liczba jest mniejsza od wylosowanej" << endl;
+		}
+		cin >> proba;
+	}
+	cout << "Gratulacje, zgadles!" << endl;
+	*/
+
+
+
+	//ZAD6c, zgadywanka w petli bez ograniczen, do-while
+	/*int liczba, proba;
+	srand(time(NULL));
+	liczba = rand() % 21;
+
+	cout << "Zgadnij jaka wylosowalem liczbe (0-20)" << endl;
+	do
+	{
+		cin >> proba;
+		if (proba > liczba)
+		{
+			cout << "Podana liczba jest wieksza od wylosowanej" << endl;
+		}
+		else if (proba < liczba)
+		{
+			cout << "Podana liczba jest mniejsza od wylosowanej" << endl;
+		}
+	} while (proba != liczba);
+	cout << "Gratulacje, zgadles!" << endl;
+	*/
+
+
+
+	//ZAD7a, zgadywanka z ograniczeniem, while
 	/*int liczba, proba,liczbaprob=0;
 	srand(time(NULL));
 	liczba = rand() % 21;
@@ -136,6 +195,103 @@ int main(int argc, char**argv)
 	}
 	cout << "Przekroczyles liczbe prob" << endl;
 	*/
+
+
+	//ZAD7b, zgadywanka z ograniczeniem, do-while
+	/*int liczba, proba, liczbaprob = 0;
+	srand(time(NULL));
+	liczba = rand() % 21;
+
+	cout << "Zgadnij jaka wylosowalem liczbe (0-20)" << endl;
+	do
+	{
+		cin >> proba;
+		liczbaprob = liczbaprob + 1;
+		if (proba == liczba)
+		{
+			cout << "Gratulacje, zgadles!" << endl;
+			break;
+		}
+		else if (proba > liczba)
+		{
+			cout << "Podana liczba jest wieksza od wylosowanej" << endl;
+		}
+		else
+		{
+			cout << "Podana liczba jest mniejsza od wylosowanej" << endl;
+		}
+	} while (liczbaprob != 6);
+	*/
+
+
+
+	//ZAD7c, zgadywanka z ograniczeniem, for
+	/*int liczba, proba;
+	srand(time(NULL));
+	liczba = rand() % 21;
+
+	cout << "Zgadnij jaka wylosowalem liczbe (0-20)- masz 6 prob" << endl;
+	for (int i = 1;i < 7; i++)
+	{
+		cin >> proba;
+		if (proba == liczba)
+		{
+			cout << "Gratulacje, zgadles!" << endl;
+			break;
+		}
+		else if (proba > liczba)
+		{
+			cout << "Podana liczba jest wieksza od wylosowanej" << endl;
+		}
+		else
+		{
+			cout << "Podana liczba jest mniejsza od wylosowanej" << endl;
+		}
+	}*/
+
+	//ZAD8, wprowadzenie liczb, wyswietlenie ich ilosci oraz liczenie sredniej, maks, min, odchylenia standardowego
+	/*double liczba,max,min,odchylenie,srednia;
+	double suma = 0;
+	double suma2 = 0;
+	int czy;
+	int licznik = 0;
+	max = -32000;
+	min = 32000;
+	odchylenie=0;
+		do
+		{
+			cout << "Podaj liczbe " << endl;
+			cin >> liczba;
+			licznik++;
+
+			suma2 = suma2 + pow(liczba, 2);
+
+			if (liczba > max)
+			{
+				max = liczba;
+			}
+
+			if (liczba < min)
+			{
+				min = liczba;
+			}
+
+			suma = suma + liczba;
+			cout << "Czy chcesz zakonczyc wpisywanie? (1-Tak, 0-Nie)" << endl;
+			cin >> czy;
+			system("cls");
+
+		} while (czy!=1);
+
+		srednia = suma / licznik;
+		odchylenie= sqrt(suma2 / licznik - pow(srednia, 2));
+
+		cout << "Ile wpisales liczb: " << licznik << endl;
+		cout << "Srednia wpisanych liczb wynosi: " << srednia << endl;
+		cout << "Minimalna z wprowadzonych liczb jest: " << min << endl;
+		cout << "Maksymalna z wprowadzonych liczb jest: " << max << endl;
+		cout << "Odchylenie standardowe wprowadzonych liczb wynosi: " << odchylenie << endl;
+		*/
 
 	return 0;
 }
